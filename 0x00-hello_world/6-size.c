45c61c764c0c62a8881b5f2c4149fbcb5fb23718
Cyphergenesis <josquilani@gmail.com>
2023-03-16 19:16:28 +0800

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+	printf("Size of a char is: %d\n", sizeof(char));
+	printf("Size of an int is: %d\n", sizeof(int));
+	printf("size of long int is: %d\n", sizeof(long int));
+	printf("Size of long long int: %d\n", sizeof(long long int));
+	return (0);
+}
