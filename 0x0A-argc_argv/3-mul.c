68cc060e5a3d781643c9a8f1ef415fc55d73feb8
Cyphergenesis <josquilani200@gmail.com>
2023-04-09 18:09:23 -0700

    updated task

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,18 +10,18 @@
 int main(int argc, char *argv[])
 {
 	int i;
-	int res = 1;
+	int  a, b;
 
-	for (i = 1; i < argc; i++)
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+
+	if (argc < 3 || argc > 3)
 	{
-		if (argc < 2)
-		{
-			printf("Error");
-			return (1);
-		}
-		res *= atoi(argv[i]);
-		printf("%d", res);
+		printf("Error");
+		return (1);
 	}
+	i = a * b;
+	printf("%d", i);
 	putchar('\n');
 	return (0);
 }
