7fa1e337cd9092d02d601bd139833792a41ba8cc
Cyphergenesis <josquilani200>
2023-05-03 02:44:37 +0100

    adds a new node at the beginning of a linked list

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds a new node at the beginning of a linked list
+ * @head: pointer to the first node in the list
+ * @n: data to insert in that new node
+ *
+ * Return: pointer to the new node, or NULL if it fails
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = (listint_t *)malloc(sizeof(listint_t));
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	if (head != NULL)
+		return (new);
+
+	return (NULL);
+
+
+}
