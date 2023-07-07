ae1baee69a3683edb4a91f1b51c20277fcf34790
Cyphergenesis <josquilani200>
2023-07-07 04:14:30 +0100

    function that deletes a hash table

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -2,19 +2,19 @@
 
 /**
  * hash_table_delete - Deletes a hash table.
- * @htb: A pointer to a hash table.
+ * @ht: A pointer to a hash table.
  */
-void hash_table_delete(hash_table_t *htb)
+void hash_table_delete(hash_table_t *ht)
 {
-	hash_table_t *head = htb;
+	hash_table_t *head = ht;
 	hash_node_t *node, *tmp;
 	unsigned long int i;
 
-	for (i = 0; i < htb->size; i++)
+	for (i = 0; i < ht->size; i++)
 	{
-		if (htb->array[i] != NULL)
+		if (ht->array[i] != NULL)
 		{
-			node = htb->array[i];
+			node = ht->array[i];
 			while (node != NULL)
 			{
 				tmp = node->next;
