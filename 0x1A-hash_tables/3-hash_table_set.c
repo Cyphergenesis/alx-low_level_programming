5fa2be7e45f1a9d8934ce45f9908155e58c375e9
Cyphergenesis <josquilani200>
2023-07-07 04:11:39 +0100

    adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,33 +2,33 @@
 
 /**
  * hash_table_set - Add or update an element in a hash table.
- * @htb: A pointer to the hash table.
+ * @ht: A pointer to the hash table.
  * @key: The key to add - cannot be an empty string.
  * @value: The value associated with key.
  *
  * Return: Upon failure - 0.
  *         Otherwise - 1.
  */
-int hash_table_set(hash_table_t *htb, const char *key, const char *value)
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new;
 	char *value_copy;
 	unsigned long int index, i;
 
-	if (htb == NULL || key == NULL || *key == '\0' || value == NULL)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
 	value_copy = strdup(value);
 	if (value_copy == NULL)
 		return (0);
 
-	index = key_index((const unsigned char *)key, htb->size);
-	for (i = index; htb->array[i]; i++)
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
 	{
-		if (strcmp(htb->array[i]->key, key) == 0)
+		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			free(htb->array[i]->value);
-			htb->array[i]->value = value_copy;
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
 			return (1);
 		}
 	}
@@ -46,8 +46,8 @@ int hash_table_set(hash_table_t *htb, const char *key, const char *value)
 		return (0);
 	}
 	new->value = value_copy;
-	new->next = htb->array[index];
-	htb->array[index] = new;
+	new->next = ht->array[index];
+	ht->array[index] = new;
 
 	return (1);
 }
