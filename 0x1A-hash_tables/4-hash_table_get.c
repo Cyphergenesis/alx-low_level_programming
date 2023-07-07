29ce121d498458a8987434ff61115bcec1db74d1
Cyphergenesis <josquilani200>
2023-07-07 04:12:35 +0100

    function that retrieves a value with a key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -3,25 +3,25 @@
 /**
  * hash_table_get - Retrieve the value associated with
  *                  a key in a hash table.
- * @htb: A pointer to the hash table.
+ * @ht: A pointer to the hash table.
  * @key: The key to get the value of.
  *
  * Return: If the key cannot be matched - NULL.
  *         Otherwise - the value associated with key in ht.
  */
-char *hash_table_get(const hash_table_t *htb, const char *key)
+char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *node;
 	unsigned long int index;
 
-	if (htb == NULL || key == NULL || *key == '\0')
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
-	index = key_index((const unsigned char *)key, htb->size);
-	if (index >= htb->size)
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
 		return (NULL);
 
-	node = htb->array[index];
+	node = ht->array[index];
 	while (node && strcmp(node->key, key) != 0)
 		node = node->next;
 
