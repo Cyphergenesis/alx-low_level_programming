0e79e5bb5007bba869b1879794afd55d7217233f
Cyphergenesis <josquilani200>
2023-07-07 04:10:21 +0100

    function that creates a hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,19 +9,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *htb;
+	hash_table_t *ht;
 	unsigned long int i;
 
-	htb = malloc(sizeof(hash_table_t));
-	if (htb == NULL)
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
 		return (NULL);
 
-	htb->size = size;
-	htb->array = malloc(sizeof(hash_node_t *) * size);
-	if (htb->array == NULL)
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
-		htb->array[i] = NULL;
+		ht->array[i] = NULL;
 
-	return (htb);
+	return (ht);
 }
